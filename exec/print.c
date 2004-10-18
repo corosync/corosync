3f4c9f558838cdd8ac3337dd615270c4d3dd9da0
Steven Dake <sdake@mvista.com>
2004-10-18 18:41:10 +0000

    Change logging to include service and description.
    (Logical change 1.96)
    
    
    git-svn-id: http://svn.fedorahosted.org/svn/corosync/trunk@353 fd59a12c-fef9-0310-b244-a6a79926bd2f

--- exec/print.c
+++ exec/print.c
@@ -31,51 +31,159 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
  * THE POSSIBILITY OF SUCH DAMAGE.
  */
+#include <assert.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdarg.h>
+#include <time.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <linux/un.h>
+
 
 #include "print.h"
 #include "parse.h"
 #include "../include/ais_types.h"
 
-/*
- * logging printf
- */
-void
-internal_log_printf (int level, char *string, ...)
+unsigned int logmode = LOG_MODE_STDERR | LOG_MODE_SYSLOG;
+char *logfile = 0;
+
+static char *log_levels[] = {
+	"[ASSERT  ]",
+	"[SECURITY]",
+	"[ERROR   ]",
+	"[WARNING ]",
+	"[NOTICE  ]",
+	"[DEBUG   ]"
+};
+
+static char *log_services[] = {
+	"[ASSERT]",
+	"[MAIN ]",
+	"[GMI  ]",
+	"[CLM  ]",
+	"[AMF  ]",
+	"[CKPT ]",
+	"[EVT  ]",
+	"[EVS  ]"
+};
+
+#define LOG_MODE_DEBUG      1
+#define LOG_MODE_TIMESTAMP  2
+#define LOG_MODE_FILE       4
+#define LOG_MODE_SYSLOG     8
+#define LOG_MODE_STDERR     16
+
+int log_syslog_fd = -1;
+FILE *log_file_fp = 0;
+
+struct sockaddr_un syslog_sockaddr = {
+	sun_family: AF_UNIX,
+	sun_path: "/dev/log"
+};
+
+int log_setup (char **error_string, int log_mode, char *log_file)
 {
-	va_list ap;
-	char newstring[1024];
+	static char error_string_response[512];
 
-	va_start(ap, string);
-	
-	sprintf (newstring, "L(%x): %s", level, string);
-	vfprintf(stderr, newstring, ap);
-
-	va_end(ap);
-}
-
-void
-internal_log_printf_checkdebug (int level, char *string, ...)
-{
-	va_list ap;
-	char newstring[1024];
-
-	va_start(ap, string);
-	
-#ifdef DEBUG
-	sprintf (newstring, "L(%x): %s", level, string);
-	vfprintf(stderr, newstring, ap);
-#else
-	if (level != LOG_LEVEL_DEBUG) {
-		sprintf (newstring, "L(%x): %s", level, string);
-		vfprintf(stderr, newstring, ap);
+	if (log_mode & LOG_MODE_SYSLOG && log_syslog_fd == -1) {
+		log_syslog_fd = socket (AF_UNIX, SOCK_DGRAM, 0);
+		if (log_syslog_fd == -1) {
+			sprintf (error_string_response,
+				"Can't create syslog socket for reason (%s).\n", strerror (errno));
+			*error_string = error_string_response;
+			return (-1);
+		}
 	}
-#endif
+
+	if (log_mode & LOG_MODE_FILE) {
+		log_file_fp = fopen (log_file, "a+");
+		if (log_file_fp == 0) {
+			sprintf (error_string_response,
+				"Can't open logfile '%s' for reason (%s).\n", logfile, strerror (errno));
+			*error_string = error_string_response;
+			return (-1);
+		}
+	}
+			
+	logmode = log_mode;
+	logfile = log_file;
+
+	return (0);
+}
+
+void log_syslog (char *log_string) {
+	struct msghdr msg_log;
+	struct iovec iov_log;
+	int res;
+
+	if (log_syslog_fd == -1) {
+		log_syslog_fd = socket (AF_UNIX, SOCK_DGRAM, 0);
+	}
+	if (log_syslog_fd == -1) {
+		return;
+	}
+	iov_log.iov_base = log_string;
+	iov_log.iov_len = strlen (log_string) + 1;
+
+	msg_log.msg_iov = &iov_log;
+	msg_log.msg_iovlen = 1;
+	msg_log.msg_name = &syslog_sockaddr;
+	msg_log.msg_namelen = sizeof (syslog_sockaddr);
+	msg_log.msg_control = 0;
+	msg_log.msg_controllen = 0;
+	msg_log.msg_flags = 0;
+
+	res = sendmsg (log_syslog_fd, &msg_log, MSG_NOSIGNAL | MSG_DONTWAIT);
+}
+
+void internal_log_printf (int logclass, char *string, ...)
+{
+	va_list ap;
+	char newstring[1024];
+	char log_string[1024];
+	char char_time[512];
+	time_t curr_time;
+	int level;
+	int service;
+
+	va_start (ap, string);
+	
+	assert (logmode != 0);
+	level = logclass >> 16;
+	service = logclass & 0xff;
+
+	if (level == LOG_LEVEL_DEBUG && ((logmode & LOG_MODE_DEBUG) == 0)) {
+		return;
+	}
+
+	if (((logmode & LOG_MODE_FILE) || (logmode & LOG_MODE_STDERR)) && 
+		(logmode & LOG_MODE_TIMESTAMP)) {
+		curr_time = time (NULL);
+		strftime (char_time, sizeof (char_time), "%b %d %k:%M:%S", localtime (&curr_time));
+		sprintf (newstring, "%s %s %s %s", char_time, log_levels[level], log_services[service], string);
+	} else {
+		sprintf (newstring, "%s %s %s", log_levels[level], log_services[service], string);
+	}
+	vsprintf (log_string, newstring, ap);
+
+	/*
+	 * Output the log data
+	 */
+	if (logmode & LOG_MODE_SYSLOG) {
+		log_syslog (log_string);
+	}
+	if (logmode & LOG_MODE_FILE && log_file_fp != 0) {
+		fprintf (log_file_fp, "%s", log_string);
+	}
+	if (logmode & LOG_MODE_STDERR) {
+		fprintf (stderr, "%s", log_string);
+	}
 
 	va_end(ap);
 }
+
 
 char *getSaNameT (SaNameT *name)
 {
