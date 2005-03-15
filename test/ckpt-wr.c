61f049cba02e024cbac85f49e92f7d0ed6788ea5
Steven Dake <sdake@mvista.com>
2005-03-15 00:46:57 +0000

    (Logical change 1.147)
    git-svn-id: http://svn.fedorahosted.org/svn/corosync/trunk@520 fd59a12c-fef9-0310-b244-a6a79926bd2f

---
+++ test/ckpt-wr.c
@@ -0,0 +1,163 @@
+/*
+ * Copyright (c) 2002-2005 MontaVista Software, Inc.
+ *
+ * All rights reserved.
+ *
+ * Author: Steven Dake (sdake@mvista.com)
+ *
+ * This software licensed under BSD license, the text of which follows:
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright notice,
+ *   this list of conditions and the following disclaimer.
+ * - Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ * - Neither the name of the MontaVista Software, Inc. nor the names of its
+ *   contributors may be used to endorse or promote products derived from this
+ *   software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/select.h>
+#include <sys/un.h>
+#include <sys/time.h>
+
+#include "ais_types.h"
+#include "saCkpt.h"
+#include "sa_error.h"
+
+#define SECONDS_TO_EXPIRE 500
+
+int ckptinv;
+void printSaNameT (SaNameT *name)
+{
+	int i;
+
+	for (i = 0; i < name->length; i++) {
+		printf ("%c", name->value[i]);
+	}
+}
+
+SaVersionT version = { 'B', 1, 1 };
+
+SaNameT checkpointName = { 16, "checkpoint-sync\0" };
+
+SaCkptCheckpointCreationAttributesT checkpointCreationAttributes = {
+	SA_CKPT_WR_ALL_REPLICAS,
+	100000,
+	SA_TIME_END,
+	5,
+	20000,
+	10
+};
+
+SaCkptSectionIdT sectionId1 = {
+	14,
+	"section ID #1"
+};
+
+SaCkptSectionIdT sectionId2 = {
+	14,
+	"section ID #2"
+};
+
+SaCkptSectionCreationAttributesT sectionCreationAttributes1 = {
+	&sectionId1,
+	SA_TIME_END
+};
+
+SaCkptSectionCreationAttributesT sectionCreationAttributes2 = {
+	&sectionId2,
+	SA_TIME_END
+};
+
+char readBuffer1[1025];
+
+char readBuffer2[1025];
+
+SaCkptIOVectorElementT ReadVectorElements[] = {
+	{
+		{
+			14,
+			"section ID #1"
+		},
+		readBuffer1,
+		sizeof (readBuffer1),
+		0, 
+		0
+	},
+	{
+		{
+			14,
+			"section ID #2"
+		},
+		readBuffer2,
+		sizeof (readBuffer2),
+		0, 
+		0
+	}
+};
+
+#define DATASIZE 127000
+char data[DATASIZE];
+SaCkptIOVectorElementT WriteVectorElements[] = {
+	{
+		{
+			14,
+			"section ID #1"
+		},
+		data, /*"written data #1, this should extend past end of old section data", */
+		DATASIZE, /*sizeof ("data #1, this should extend past end of old section data") + 1, */
+		0, //5, 
+		0
+	}
+};
+
+SaCkptCallbacksT callbacks = {
+ 	0,
+	0
+};
+
+int main (void) {
+	SaCkptHandleT ckptHandle;
+	SaCkptCheckpointHandleT checkpointHandle;
+	SaAisErrorT error;
+	
+	error = saCkptInitialize (&ckptHandle, &callbacks, &version);
+	
+	error = saCkptCheckpointOpen (ckptHandle,
+		&checkpointName,
+		&checkpointCreationAttributes,
+		SA_CKPT_CHECKPOINT_READ|SA_CKPT_CHECKPOINT_WRITE,
+		0,
+		&checkpointHandle);
+	printf ("%s: initial open of checkpoint\n",
+		get_test_output (error, SA_AIS_OK));
+
+	error = saCkptSectionCreate (checkpointHandle,
+		&sectionCreationAttributes1,
+		"Initial Data #0",
+		strlen ("Initial Data #0") + 1);
+
+	return (0);
+}
