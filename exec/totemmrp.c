f87bf5d029141da18ba3760ccc53987ab2ddb5e7
Steven Dake <sdake@redhat.com>
2009-03-10 11:42:10 +0000

    Fix missing prototypes in totemmrp.
    git-svn-id: http://svn.fedorahosted.org/svn/corosync/trunk@1820 fd59a12c-fef9-0310-b244-a6a79926bd2f

--- exec/totemmrp.c
+++ exec/totemmrp.c
@@ -63,6 +63,19 @@
 
 hdb_handle_t totemsrp_handle_in;
 
+void totemmrp_deliver_fn (
+	unsigned int nodeid,
+	struct iovec *iovec,
+	int iov_len,
+	int endian_conversion_required);
+
+void totemmrp_confchg_fn (
+	enum totem_configuration_type configuration_type,
+	unsigned int *member_list, int member_list_entries,
+	unsigned int *left_list, int left_list_entries,
+	unsigned int *joined_list, int joined_list_entries,
+	struct memb_ring_id *ring_id);
+
 void (*pg_deliver_fn) (
 	unsigned int nodeid,
 	struct iovec *iovec,
