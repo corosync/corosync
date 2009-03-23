50cb2848752c8703d57d12df9290f68c7a4bbd46
Steven Dake <sdake@redhat.com>
2009-03-23 22:15:42 +0000

    Tagging corosync-0.95.
    git-svn-id: http://svn.fedorahosted.org/svn/corosync/tags/corosync-0.95@1897 fd59a12c-fef9-0310-b244-a6a79926bd2f

---
+++ include/corosync/cs_config.h
@@ -0,0 +1,6 @@
+/* include/corosync/cs_config.h.  Generated from cs_config.h.in by configure.  */
+/* Location of lcrso plugins */
+#define LCRSODIR "/usr/libexec/lcrso"
+
+/* Location of sockets */
+#define SOCKETDIR "/var/run"
