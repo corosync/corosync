596e49cd7e92f947f6dc17d8f5082f6592430b9d
Jim Meyering <jim@meyering.net>
2009-03-24 10:38:52 +0000

    avoid 8 warnings
    * exec/totemconfig.c (objdb_get_string): Make "key" const.
    
    git-svn-id: http://svn.fedorahosted.org/svn/corosync/trunk@1904 fd59a12c-fef9-0310-b244-a6a79926bd2f

--- exec/totemconfig.c
+++ exec/totemconfig.c
@@ -91,7 +91,7 @@ static void add_totem_config_notification(
 static inline int objdb_get_string (
 	struct objdb_iface_ver0 *objdb,
 	hdb_handle_t object_service_handle,
-	char *key, char **value)
+	const char *key, char **value)
 {
 	int res;
 
