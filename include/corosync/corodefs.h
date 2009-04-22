92a77fcf9aeac5e6860d3f3eacde1ab9fee83fa4
Steven Dake <sdake@redhat.com>
2009-04-22 09:03:59 +0000

    Add missing header from last commit.
    git-svn-id: http://svn.fedorahosted.org/svn/corosync/trunk@2120 fd59a12c-fef9-0310-b244-a6a79926bd2f

---
+++ include/corosync/corodefs.h
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) 2009 Red Hat, Inc.
+ *
+ * All rights reserved.
+ *
+ * Author: Steven Dake (sdake@redhat.com)
+ *
+ * This software licensed under BSD license, the text of which follows:
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright notice,
+ *   this list of conditions and the following disclaimer.
+ * - Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ * - Neither the name of the MontaVista Software, Inc. nor the names of its
+ *   contributors may be used to endorse or promote products derived from this
+ *   software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef CORODEFS_H_DEFINED
+#define CORODEFS_H_DEFINED
+
+#include <config.h>
+#include <netinet/in.h>
+
+#define COROSYNC_SOCKET_NAME	"corosync.ipc"
+
+#define IPC_REQUEST_SIZE	8192*128
+#define IPC_RESPONSE_SIZE	8192*128
+#define IPC_DISPATCH_SIZE	8192*128
+
+enum corosync_service_types {
+	EVS_SERVICE = 0,
+	CLM_SERVICE = 1,
+	AMF_SERVICE = 2,
+	CKPT_SERVICE = 3,
+	EVT_SERVICE = 4,
+	LCK_SERVICE = 5,
+	MSG_SERVICE = 6,
+	CFG_SERVICE = 7,
+	CPG_SERVICE = 8,
+	CONFDB_SERVICE = 10,
+	QUORUM_SERVICE = 11,
+	PLOAD_SERVICE = 12,
+	TMR_SERVICE = 13,
+	VOTEQUORUM_SERVICE = 14,
+};
+
+#define PROCESSOR_COUNT_MAX	384
+
+#define TOTEMIP_ADDRLEN (sizeof(struct in6_addr))
+
+#endif /* CORODEFS_H_DEFINED */
