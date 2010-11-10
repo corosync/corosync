a544e3fe21af111ccd35f80b3fc5324abdb46812
Angus Salkeld <asalkeld@redhat.com>
2010-11-10 19:38:35 +1100

    libqb: expose a libqb error converter
    Signed-off-by: Angus Salkeld <asalkeld@redhat.com>
    Reviewed-by: Steven Dake <sdake@redhat.com>

--- include/corosync/corotypes.h
+++ include/corosync/corotypes.h
@@ -40,6 +40,7 @@
 #else
 #include <sys/types.h>
 #endif
+#include <errno.h>
 #include <time.h>
 #include <sys/time.h>
 
@@ -130,6 +131,86 @@ static inline uint64_t cs_timestamp_get(void)
 #endif
 
 	return result;
+}
+
+#define errno_to_cs qb_to_cs_error
+static inline cs_error_t qb_to_cs_error (int result)
+{
+	int32_t res;
+	cs_error_t err = CS_ERR_LIBRARY;
+
+	if (result >= 0) {
+		return CS_OK;
+	}
+	res = -result;
+
+	switch (res) {
+	case 0:
+		err = CS_OK;
+		break;
+	case EBADF:
+		err = CS_ERR_BAD_HANDLE;
+		break;
+	case ENOMEM:
+		err = CS_ERR_NO_MEMORY;
+		break;
+	case EAGAIN:
+		err = CS_ERR_TRY_AGAIN;
+		break;
+	case EBADE:
+		err = CS_ERR_FAILED_OPERATION;
+		break;
+	case ETIME:
+		err = CS_ERR_TIMEOUT;
+		break;
+	case EINVAL:
+		err = CS_ERR_INVALID_PARAM;
+		break;
+	case EBUSY:
+		err = CS_ERR_BUSY;
+		break;
+	case EACCES:
+		err = CS_ERR_ACCESS;
+		break;
+	case ECONNREFUSED:
+		err = CS_ERR_SECURITY;
+		break;
+	case EOVERFLOW:
+		err = CS_ERR_NAME_TOO_LONG;
+		break;
+	case EEXIST:
+		err = CS_ERR_EXIST;
+		break;
+	case ENOBUFS:
+		err = CS_ERR_QUEUE_FULL;
+		break;
+	case ENOSPC:
+		err = CS_ERR_NO_SPACE;
+		break;
+	case EINTR:
+		err = CS_ERR_INTERRUPT;
+		break;
+	case ENOENT:
+	case ENODEV:
+		err = CS_ERR_NOT_EXIST;
+		break;
+	case ENOSYS:
+	case ENOTSUP:
+		err = CS_ERR_NOT_SUPPORTED;
+		break;
+	case EBADMSG:
+		err = CS_ERR_MESSAGE_ERROR;
+		break;
+	case EMSGSIZE:
+	case E2BIG:
+		err = CS_ERR_TOO_BIG;
+		break;
+	default:
+		err = CS_ERR_LIBRARY;
+		break;
+	}
+
+	return err;
 }
 
 
