e93e6ce567ec7e9aa2b9565d04a867c24b275d0c
Angus Salkeld <asalkeld@redhat.com>
2011-01-11 10:55:56 +1100

    fix timersub warning on freebsd
    Make them all protected by #ifndef timersub
    
    Signed-off-by: Angus Salkeld <asalkeld@redhat.com>
    Reviewed-by: Steven Dake <sdake@redhat.com>

--- test/cpgbenchzc.c
+++ test/cpgbenchzc.c
@@ -54,15 +54,15 @@
 #include <corosync/corotypes.h>
 #include <corosync/cpg.h>
 
-#ifdef COROSYNC_SOLARIS
-#define timersub(a, b, result)						\
-    do {								\
-	(result)->tv_sec = (a)->tv_sec - (b)->tv_sec;			\
-	(result)->tv_usec = (a)->tv_usec - (b)->tv_usec;		\
-	if ((result)->tv_usec < 0) {					\
-	    --(result)->tv_sec;						\
-	    (result)->tv_usec += 1000000;				\
-	}								\
+#ifndef timersub
+#define timersub(a, b, result)					\
+    do {							\
+	(result)->tv_sec = (a)->tv_sec - (b)->tv_sec;		\
+	(result)->tv_usec = (a)->tv_usec - (b)->tv_usec;	\
+	if ((result)->tv_usec < 0) {				\
+	    --(result)->tv_sec;					\
+	    (result)->tv_usec += 1000000;			\
+	}							\
     } while (0)
 #endif
 
