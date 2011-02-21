b9ae869d227e35851d454bb8ce2aa8205884836f
Jan Friesse <jfriesse@redhat.com>
2011-02-21 13:14:21 +0100

    logsys: Don't leak fd on successful fdata dump
    Signed-off-by: Jan Friesse <jfriesse@redhat.com>
    Reviewed-by: Steven Dake <sdake@redhat.com>

--- exec/logsys.c
+++ exec/logsys.c
@@ -1642,7 +1642,8 @@ int logsys_log_rec_store (const char *filename)
 	if (written_size != ((flt_data_size + 3) * sizeof (uint32_t))) { 
 		goto error_exit;
 	}
-	
+
+	close (fd);
 	return (0);
 
 error_exit:
