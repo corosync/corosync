a7b4e7e0452b93520fa69d2108ba116e1b3f8e22
Steven Dake <sdake@redhat.com>
2012-02-13 11:19:29 -0700

    Update copyright dates on include/totem files
    Signed-off-by: Steven Dake <sdake@redhat.com>
    Reviewed-by: Fabio Di Nitto <fdinitto@redhat.com>

--- include/corosync/totem/totem.h
+++ include/corosync/totem/totem.h
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2005 MontaVista Software, Inc.
- * Copyright (c) 2006-2009 Red Hat, Inc.
+ * Copyright (c) 2006-2012 Red Hat, Inc.
  *
  * Author: Steven Dake (sdake@redhat.com)
  *
