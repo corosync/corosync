11fb909036f532d78246b5587be3083f4b51a0d5
Angus Salkeld <asalkeld@redhat.com>
2012-02-13 21:45:29 +1100

    TEST: remove unused code.
    Signed-off-by: Angus Salkeld <asalkeld@redhat.com>
    Reviewed-by: Steven Dake <sdake@redhat.com>

--- test/sa_error.h
+++
@@ -1,5 +0,0 @@
-extern int get_sa_error(cs_error_t error, char *str, int len);
-
-extern char *get_sa_error_b (cs_error_t error);
-
-extern char *get_test_output (cs_error_t result, cs_error_t expected);
