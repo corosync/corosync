corosync/corosync

commit a853b6180e9832fa763ea796f310dc3de5718324

Correctly handle point-to-point IP
Main difference between IFA_LOCAL and IFA_ADDRESS:

IFA_ADDRESS is prefix address, rather than local interface address.
It makes no difference for normally configured broadcast interfaces,
but for point-to-point IFA_ADDRESS is DESTINATION address,
local address is supplied in IFA_LOCAL attribute.

We are now using IFA_LOCAL insted of IFA_ADDRESS to correctly find out
local address.

Signed-off-by: Jan Friesse <jfriesse@redhat.com>
Reviewed-by: Fabio M. Di Nitto <fdinitto@redhat.com>

exec/totemip.c
@@ -596,9 +596,11 @@ int totemip_iface_check(struct totem_ip_address *bindnet,
 
 				parse_rtattr(tb, IFA_MAX, IFA_RTA(ifa), len);
 
-				memcpy(ipaddr.addr, RTA_DATA(tb[IFA_ADDRESS]), TOTEMIP_ADDRLEN);
-				if (totemip_equal(&ipaddr, bindnet)) {
-					found_if = 1;
+				if (tb[IFA_LOCAL]) {
+					memcpy(ipaddr.addr, RTA_DATA(tb[IFA_LOCAL]), TOTEMIP_ADDRLEN);
+					if (totemip_equal(&ipaddr, bindnet)) {
+						found_if = 1;
+					}
 				}
 
 				/* If the address we have is an IPv4 network address, then

commit d76759ec26ecaeb9cc01f49e9eb0749b61454d27

totemip: Properly detect ipv6 address
Commit a853b618 introduced regression in finding IPv6 address. For IPv6,
IFA_ADDRESS must be checked, instead of IFA_LOCAL, because IFA_LOCAL is
returned empty.

Signed-off-by: Jan Friesse <jfriesse@redhat.com>
Reviewed-by: Fabio M. Di Nitto <fdinitto@redhat.com>

exec/totemip.c
@@ -598,6 +598,13 @@ int totemip_iface_check(struct totem_ip_address *bindnet,
 
 				parse_rtattr(tb, IFA_MAX, IFA_RTA(ifa), len);
 
+				if (ifa->ifa_family == AF_INET6 && tb[IFA_ADDRESS]) {
+					memcpy(ipaddr.addr, RTA_DATA(tb[IFA_ADDRESS]), TOTEMIP_ADDRLEN);
+					if (totemip_equal(&ipaddr, bindnet)) {
+						found_if = 1;
+						exact_match_found = 1;
+					}
+				}
 				if (tb[IFA_LOCAL]) {
 					memcpy(ipaddr.addr, RTA_DATA(tb[IFA_LOCAL]), TOTEMIP_ADDRLEN);
 					if (totemip_equal(&ipaddr, bindnet)) {