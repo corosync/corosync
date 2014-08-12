4c717942cf5f35902be630f393b81a03a81bb194
Jan Friesse <jfriesse@redhat.com>
2014-08-12 15:56:08 +0200

    totemnet: Add totemnet_member_set_active
    totemnet_member_set_active together with transport specific
    member_set_active makes possible for totemnet (and more interestingly
    transport) to be informed about membership changes.
    
    Signed-off-by: Jan Friesse <jfriesse@redhat.com>
    Reviewed-by: Christine Caulfield <ccaulfie@redhat.com>

--- exec/totemnet.c
+++ exec/totemnet.c
@@ -130,6 +130,11 @@ struct transport {
 	int (*member_remove) (
 		void *transport_context,
 		const struct totem_ip_address *member);
+
+	int (*member_set_active) (
+		void *transport_context,
+		const struct totem_ip_address *member,
+		int active);
 };
 
 struct transport transport_entries[] = {
@@ -488,6 +493,24 @@ extern int totemnet_member_remove (
 		res = instance->transport->member_remove (
 			instance->transport_context,
 			member);
+	}
+
+	return (res);
+}
+
+int totemnet_member_set_active (
+	void *net_context,
+	const struct totem_ip_address *member,
+	int active)
+{
+	struct totemnet_instance *instance = (struct totemnet_instance *)net_context;
+	unsigned int res = 0;
+
+	if (instance->transport->member_set_active) {
+		res = instance->transport->member_set_active (
+			instance->transport_context,
+			member,
+			active);
 	}
 
 	return (res);
