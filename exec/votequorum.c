a71ec5d95de35a67a4524ecf3076bd9ba27caccb
Christine Caulfield <ccaulfie@redhat.com>
2015-10-22 11:45:26 +0100

    votequorum: Don't send multiple callbacks when nodes join
    This patch aligns the votequorum callbacks so that they are
    the same as the quorum ones. Previously it was quite common
    for votequorum to send one callback for every node in the cluster
    when a single new node joined (because it sent one for every
    nodeinfo message it received).
    
    This new system makes much more sense in itself and being
    consistent with the internal quorum is also an advantage!
    
    Signed-off-by: Christine Caulfield <ccaulfie@redhat.com>
    Reviewed-by: Jan Friesse <jfriesse@redhat.com>

--- exec/votequorum.c
+++ exec/votequorum.c
@@ -1054,6 +1054,7 @@ static void are_we_quorate(unsigned int total_votes)
 	    (sync_in_progress == 0)) {
 		quorum_callback(quorum_members, quorum_members_entries,
 				cluster_is_quorate, &quorum_ringid);
+		votequorum_exec_send_quorum_notification(NULL, 0L);
 	}
 
 	LEAVE();
@@ -1119,8 +1120,6 @@ static void recalculate_quorum(int allow_decrease, int by_current_nodes)
 
 	quorum = calculate_quorum(allow_decrease, cluster_members, &total_votes);
 	are_we_quorate(total_votes);
-
-	votequorum_exec_send_quorum_notification(NULL, 0L);
 
 	LEAVE();
 }
@@ -2352,6 +2351,8 @@ static void votequorum_sync_activate (void)
 	recalculate_quorum(0, 0);
 	quorum_callback(quorum_members, quorum_members_entries,
 			cluster_is_quorate, &quorum_ringid);
+	votequorum_exec_send_quorum_notification(NULL, 0L);
+
 	sync_in_progress = 0;
 }
 
