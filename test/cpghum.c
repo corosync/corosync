b3d262e018ccd4c17691d9d2d2626324f58dc693
Ruben Kerkhof <ruben@rubenkerkhof.com>
2015-12-15 12:31:28 +0100

    Fix type of recv_crc
    Fixes build on FreeBSD which doesn't have ulong

--- test/cpghum.c
+++ test/cpghum.c
@@ -116,7 +116,7 @@ static void cpg_bm_deliver_fn (
 {
 	int *value = msg;
 	uLong crc=0;
-	ulong recv_crc = value[1] & 0xFFFFFFFF;
+	uLong recv_crc = value[1] & 0xFFFFFFFF;
 
 	packets_recvd++;
 	g_recv_length = msg_len;
