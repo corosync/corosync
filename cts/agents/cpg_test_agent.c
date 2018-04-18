67c644e69baf73b4c61761859f1ae4ad430d4221
Ferenc Wágner <wferi@debian.org>
2018-04-18 18:17:41 +0200

    NSS_NoDB_Init: the parameter is reserved, must be NULL
    Signed-off-by: Ferenc Wágner <wferi@debian.org>
    Reviewed-by: Jan Friesse <jfriesse@redhat.com>

--- cts/agents/cpg_test_agent.c
+++ cts/agents/cpg_test_agent.c
@@ -789,7 +789,7 @@ main(int argc, char *argv[])
 	qb_list_init (&msg_log_head);
 	qb_list_init (&config_chg_log_head);
 
-	if (NSS_NoDB_Init(".") != SECSuccess) {
+	if (NSS_NoDB_Init(NULL) != SECSuccess) {
 		qb_log(LOG_ERR, "Couldn't initialize nss");
 		exit (0);
 	}
