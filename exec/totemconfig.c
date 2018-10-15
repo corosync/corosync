corosync/corosync

commit 429209f4aa3c55504a49833e0004489f241e7819

totemconfig: Check for things that cannot be changed on the fly
There are a few things in the interface that cannot be changed on the
fly. Warn about them and tell the user that these things need to be done
in two steps and why.

Signed-off-by: Christine Caulfield <ccaulfie@redhat.com>
Reviewed-by: Jan Friesse <jfriesse@redhat.com>

exec/totemconfig.c
@@ -1071,6 +1071,41 @@ static void reconfigure_links(struct totem_config *totem_config)
 	}
 }
 
+/* Check for differences in config that can't be done on-the-fly and print an error */
+static void check_things_have_not_changed(struct totem_config *totem_config)
+{
+	int i,j;
+	const char *ip_str;
+	char addr_buf[INET6_ADDRSTRLEN];
+	int changed = 0;
+
+	for (i = 0; i<INTERFACE_MAX; i++) {
+		if (totem_config->interfaces[i].configured) {
+			if (totem_config->interfaces[i].knet_transport !=
+			    totem_config->orig_interfaces[i].knet_transport) {
+				log_printf(LOGSYS_LEVEL_ERROR, "New config has different knet transport for link %d. Internal value was NOT changed.\n", i);
+				changed = 1;
+			}
+			for (j=0; j<totem_config->interfaces[i].member_count; j++) {
+				if (memcmp(&totem_config->interfaces[i].member_list[j],
+					   &totem_config->orig_interfaces[i].member_list[j],
+					   sizeof(struct totem_ip_address))) {
+
+					ip_str = totemip_print(&totem_config->orig_interfaces[i].member_list[j]);
+					strcpy(addr_buf, ip_str);
+					log_printf(LOGSYS_LEVEL_ERROR, "new config has different address for link %d (addr changed from %s to %s). Internal value was NOT changed.\n", i, addr_buf, totemip_print(&totem_config->interfaces[i].member_list[j]));
+					changed = 1;
+				}
+			}
+		}
+	}
+
+	if (changed) {
+		log_printf(LOGSYS_LEVEL_ERROR, "To reconfigure and interface it must be deleted and recreated. A working interface needs to be available to corosync at all times");
+	}
+}
+
+
 static void put_nodelist_members_to_config(struct totem_config *totem_config, int reload)
 {
 	icmap_iter_t iter, iter2;
@@ -1152,6 +1187,8 @@ static void put_nodelist_members_to_config(struct totem_config *totem_config, in
 		reconfigure_links(totem_config);
 
 		memcpy(new_interfaces, totem_config->interfaces, sizeof (struct totem_interface) * INTERFACE_MAX);
+
+		check_things_have_not_changed(totem_config);
 
 		compute_interfaces_diff(totem_config->orig_interfaces, new_interfaces);
 

commit 307aafb86b67adb0f8f908f21a86d3107e06fa59

config: Fix crash in reload if new interfaces are added
This is a bug I seem to have introduced in
429209f4aa3c55504a49833e0004489f241e7819 where we compare links
for changes. if a new node was added on an existing link then it
was compared against a non-existant one in the previous configuration.
We now only compare nodes that are in both interfaces.

As I needed min() for this function, I moved it from individual
.c files into util.h so we only have one copy.

And the error message was fixed.

Signed-off-by: Christine Caulfield <ccaulfie@redhat.com>

exec/totemconfig.c
@@ -1137,7 +1137,7 @@ static void check_things_have_not_changed(struct totem_config *totem_config)
 				log_printf(LOGSYS_LEVEL_ERROR, "New config has different knet transport for link %d. Internal value was NOT changed.\n", i);
 				changed = 1;
 			}
-			for (j=0; j<totem_config->interfaces[i].member_count; j++) {
+			for (j=0; j < min(totem_config->interfaces[i].member_count, totem_config->orig_interfaces[i].member_count); j++) {
 				if (memcmp(&totem_config->interfaces[i].member_list[j],
 					   &totem_config->orig_interfaces[i].member_list[j],
 					   sizeof(struct totem_ip_address))) {
@@ -1152,7 +1152,7 @@ static void check_things_have_not_changed(struct totem_config *totem_config)
 	}
 
 	if (changed) {
-		log_printf(LOGSYS_LEVEL_ERROR, "To reconfigure and interface it must be deleted and recreated. A working interface needs to be available to corosync at all times");
+		log_printf(LOGSYS_LEVEL_ERROR, "To reconfigure an interface it must be deleted and recreated. A working interface needs to be available to corosync at all times");
 	}
 }
 

exec/totempg.c
@@ -108,9 +108,8 @@
 #define LOGSYS_UTILS_ONLY 1
 #include <corosync/logsys.h>
 
+#include "util.h"
 #include "totemsrp.h"
-
-#define min(a,b) ((a) < (b)) ? a : b
 
 struct totempg_mcast_header {
 	short version;

exec/util.c
@@ -136,8 +136,6 @@ void _corosync_exit_error (
 	exit (err);
 }
 
-#define min(a,b) ((a) < (b) ? (a) : (b))
-
 char *getcs_name_t (cs_name_t *name)
 {
 	static char ret_name[CS_MAX_NAME_LENGTH];

exec/util.h
@@ -63,6 +63,7 @@ enum e_corosync_done {
 	COROSYNC_DONE_PLOAD = 99
 };
 
+#define min(a,b) ((a) < (b) ? (a) : (b))
 
 /**
  * Compare two names.  returns non-zero on match.