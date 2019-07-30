41a7b18dedf2da15b5547cede08869b570cfb9ab
Jan Friesse <jfriesse@redhat.com>
2019-07-30 14:24:32 +0200

    notifyd: Don't dereference NULL key_name
    This problem shouldn't really happen, but better safe than sorry.
    
    Signed-off-by: Jan Friesse <jfriesse@redhat.com>

--- tools/corosync-notifyd.c
+++ tools/corosync-notifyd.c
@@ -238,6 +238,7 @@ static void _cs_cmap_members_key_changed (
 
 	if (NULL == key_name) {
 		qb_log(LOG_ERR, "key_name: nil");
+		return ;
 	}
 
 	res = sscanf(key_name, "runtime.members.%u.%s", &nodeid, tmp_key);
