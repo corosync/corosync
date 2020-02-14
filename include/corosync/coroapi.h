02153b0be17798932e365c43007874c077084ffc
Jan Friesse <jfriesse@redhat.com>
2020-02-14 09:42:08 +0100

    totemip: Add support for sin6_scope_id
    sin6_scope_id was not present in totemip structure making impossible to
    use link-local ipv6 address.
    
    Patch adds sin6_scope_id and changes convert/copy functions to use it
    (formally also comparator functions should be changed, but it seems to
    cause more harm and it is not really needed).
    
    This makes corosync work with link-local addresses fine for both UDPU
    and Knet transport as long as interface specification is used (so
    fe80::xxxx:xxxx:xxxx:xxxx%eth0).
    
    Signed-off-by: Jan Friesse <jfriesse@redhat.com>

--- include/corosync/coroapi.h
+++ include/corosync/coroapi.h
@@ -112,6 +112,7 @@ struct totem_ip_address {
 	unsigned int   nodeid;
 	unsigned short family;
 	unsigned char  addr[TOTEMIP_ADDRLEN];
+	unsigned int   sin6_scope_id;
 } __attribute__((packed));
 #endif
 
