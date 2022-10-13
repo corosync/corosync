75af6caf912fb84ea4199642498d2065778813c5
Christine Caulfield <ccaulfie@redhat.com>
2022-10-13 12:42:31 +0100

    Add Rust bindings

---
+++ bindings/rust/tests/src/bin/cfg-test.rs
@@ -0,0 +1,136 @@
+// Test the CFG library. Requires that corosync is running and that we are root.
+
+extern crate rust_corosync as corosync;
+use corosync::{cfg, NodeId};
+
+use std::thread::spawn;
+
+fn dispatch_thread(handle: cfg::Handle) {
+    loop {
+        if cfg::dispatch(handle, corosync::DispatchFlags::One).is_err() {
+            return;
+        }
+    }
+}
+
+// Test the shutdown callback
+fn shutdown_check_fn(handle: &cfg::Handle, _flags: u32) {
+    println!("in shutdown callback");
+
+    // DON'T shutdown corosync - we're just testing
+    if let Err(e) = cfg::reply_to_shutdown(*handle, cfg::ShutdownReply::No) {
+        println!("Error in CFG replyto_shutdown: {}", e);
+    }
+}
+
+fn main() {
+    // Initialise the callbacks data
+    let cb = cfg::Callbacks {
+        corosync_cfg_shutdown_callback_fn: Some(shutdown_check_fn),
+    };
+
+    let handle = match cfg::initialize(&cb) {
+        Ok(h) => {
+            println!("cfg initialized.");
+            h
+        }
+        Err(e) => {
+            println!("Error in CFG init: {}", e);
+            return;
+        }
+    };
+
+    // Open two handles to CFG so that the second one can refuse shutdown
+    let handle2 = match cfg::initialize(&cb) {
+        Ok(h) => {
+            println!("cfg2 initialized.");
+            h
+        }
+        Err(e) => {
+            println!("Error in CFG init: {}", e);
+            return;
+        }
+    };
+
+    match cfg::track_start(handle2, cfg::TrackFlags::None) {
+        Ok(_) => {
+            // Run handle2 dispatch in its own thread
+            spawn(move || dispatch_thread(handle2));
+        }
+        Err(e) => {
+            println!("Error in CFG track_start: {}", e);
+        }
+    };
+
+    let local_nodeid = {
+        match cfg::local_get(handle) {
+            Ok(n) => {
+                println!("Local nodeid is {}", n);
+                Some(n)
+            }
+            Err(e) => {
+                println!("Error in CFG local_get: {}", e);
+                None
+            }
+        }
+    };
+
+    // Test node_status_get.
+    // node status for the local node looks odd (cos it's the loopback connection), so
+    // we try for a node ID one less or more than us just to get output that looks
+    // sensible to the user.
+    if let Some(our_nodeid) = local_nodeid {
+        let us_plus1 = NodeId::from(u32::from(our_nodeid) + 1);
+        let us_less1 = NodeId::from(u32::from(our_nodeid) - 1);
+        let mut res = cfg::node_status_get(handle, us_plus1, cfg::NodeStatusVersion::V1);
+        if let Err(e) = res {
+            println!("Error from node_status_get on nodeid {}: {}", us_plus1, e);
+            res = cfg::node_status_get(handle, us_less1, cfg::NodeStatusVersion::V1);
+        };
+        match res {
+            Ok(ns) => {
+                println!("Node Status for nodeid {}", ns.nodeid);
+                println!("   reachable: {}", ns.reachable);
+                println!("   remote: {}", ns.remote);
+                println!("   onwire_min: {}", ns.onwire_min);
+                println!("   onwire_max: {}", ns.onwire_max);
+                println!("   onwire_ver: {}", ns.onwire_ver);
+                for (ls_num, ls) in ns.link_status.iter().enumerate() {
+                    if ls.enabled {
+                        println!("   Link {}", ls_num);
+                        println!("      connected: {}", ls.connected);
+                        println!("      mtu: {}", ls.mtu);
+                        println!("      src: {}", ls.src_ipaddr);
+                        println!("      dst: {}", ls.dst_ipaddr);
+                    }
+                }
+            }
+            Err(e) => {
+                println!(
+                    "Error in CFG node_status get: {} (tried nodeids {} & {})",
+                    e, us_plus1, us_less1
+                );
+            }
+        }
+    }
+
+    // This should not shutdown corosync because the callback on handle2 will refuse it.
+    match cfg::try_shutdown(handle, cfg::ShutdownFlags::Request) {
+        Ok(_) => {
+            println!("CFG try_shutdown suceeded, should return busy");
+        }
+        Err(e) => {
+            if e != corosync::CsError::CsErrBusy {
+                println!("Error in CFG try_shutdown: {}", e);
+            }
+        }
+    }
+
+    // Wait for events
+    loop {
+        if cfg::dispatch(handle, corosync::DispatchFlags::One).is_err() {
+            break;
+        }
+    }
+    println!("ERROR: Corosync quit");
+}
---
+++ build-aux/rust.mk
@@ -0,0 +1,113 @@
+#
+# Copyright (C) 2021-2022 Red Hat, Inc.  All rights reserved.
+#
+# Author: Fabio M. Di Nitto <fabbione@kronosnet.org>
+#
+# This software licensed under GPL-2.0+
+#
+
+RUST_COMMON = \
+	      build.rs.in
+
+RUST_SRCS = $(RUST_SHIP_SRCS) $(RUST_BUILT_SRCS)
+
+%.rlib: $(RUST_SRCS) Cargo.toml build.rs
+	PKG_CONFIG_PATH=$(PKG_CONFIG_PATH) $(CARGO) build $(RUST_FLAGS)
+
+%-test: $(RUST_SRCS) Cargo.toml build.rs
+	PKG_CONFIG_PATH=$(PKG_CONFIG_PATH) $(CARGO) build $(RUST_FLAGS)
+
+build.rs: build.rs.in
+	rm -f $@ $@-t
+	cat $^ | sed \
+		-e 's#@ABSTOPLEVELSRC@#$(abs_top_srcdir)#g' \
+		-e 's#@ABSTOPLEVELBUILD@#$(abs_top_builddir)#g' \
+		> $@-t
+	chmod a-w $@-t
+	mv $@-t $@
+	rm -f $@-t
+
+cargo-tree-prep:
+	if [ "${abs_builddir}" != "${abs_srcdir}" ]; then \
+		echo "Generating builddir out-of-tree rust symlinks"; \
+		src_realpath=$(shell realpath ${abs_srcdir}); \
+		for i in `find "$$src_realpath/" -type d | \
+			grep -v "${abs_builddir}" | \
+			sed -e 's#^'$$src_realpath'/##g'`; do \
+			$(MKDIR_P) ${abs_builddir}/$${i}; \
+		done; \
+		find "$$src_realpath/" -type f | { while read src; do \
+			process=no; \
+			copy=no; \
+			case $$src in \
+				${abs_builddir}*) \
+					;; \
+				*Makefile.*|*.in) \
+					;; \
+				*) \
+					process=yes; \
+					;; \
+			esac ; \
+			dst=`echo $$src | sed -e 's#^'$$src_realpath'/##g'`; \
+			if [ $${process} == yes ]; then \
+				rm -f ${abs_builddir}/$$dst; \
+				$(LN_S) $$src ${abs_builddir}/$$dst; \
+			fi; \
+			if [ $${copy} == yes ]; then \
+				rm -f ${abs_builddir}/$$dst; \
+				cp $$src ${abs_builddir}/$$dst; \
+				chmod u+w ${abs_builddir}/$$dst; \
+			fi; \
+		done; }; \
+	fi
+
+cargo-clean:
+	-$(CARGO) clean
+	rm -rf Cargo.lock $(RUST_BUILT_SRCS) build.rs target/
+	if [ "${abs_builddir}" != "${abs_srcdir}" ]; then \
+		echo "Cleaning out-of-tree rust symlinks" ; \
+		find "${abs_builddir}/" -type l -delete; \
+		find "${abs_builddir}/" -type d -empty -delete; \
+	fi
+
+clippy-check:
+	$(CARGO) clippy --verbose --all-features -- -D warnings
+
+format-check:
+	if [ "${abs_builddir}" = "${abs_srcdir}" ]; then \
+		$(CARGO) fmt --all --check; \
+	else \
+		echo "!!!!! WARNING: skipping format check !!!!!"; \
+	fi
+
+doc-check:
+	$(CARGO) doc --verbose --all-features
+
+publish-check:
+	if [ -f "${abs_srcdir}/README.md" ]; then \
+		$(CARGO) publish --dry-run; \
+	fi
+
+crates-publish:
+	if [ -f "${abs_srcdir}/README.md" ]; then \
+		bindingname=`cat Cargo.toml | grep ^name | sed -e 's#.*= ##g' -e 's#"##g'` && \
+		cratesver=`cargo search $$bindingname | grep "^$$bindingname " | sed -e 's#.*= ##g' -e 's#"##g' -e 's/\+.*//g'` && \
+		testver=`echo $(localver) | sed -e 's/\+.*//g'` && \
+		if [ "$$cratesver" != "$$testver" ]; then \
+			$(CARGO) publish; \
+		fi; \
+	fi
+
+crates-check:
+	if [ -f "${abs_srcdir}/README.md" ]; then \
+		bindingname=`cat Cargo.toml | grep ^name | sed -e 's#.*= ##g' -e 's#"##g'` && \
+		cratesver=`cargo search $$bindingname | grep "^$$bindingname " | sed -e 's#.*= ##g' -e 's#"##g' -e 's/\+.*//g'` && \
+		testver=`echo $(localver) | sed -e 's/\+.*//g'` && \
+		if [ "$$cratesver" != "$$testver" ]; then \
+			echo "!!!!! WARNING !!!!!"; \
+			echo "!!!!! WARNING: $$bindingname local version ($$testver) is higher than the current published one on crates.io ($$cratesver)"; \
+			echo "!!!!! WARNING !!!!!"; \
+		fi; \
+	fi
+
+check-local: clippy-check format-check doc-check crates-check publish-check
