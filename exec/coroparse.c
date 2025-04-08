30f251defd3379fd675b9d962f64d1d90bcdc218
Fabian Grünbichler <f.gruenbichler@proxmox.com>
2025-04-08 11:47:22 +0200

    conf: add system.allow_knet_asymmetric_routes
    and set the corresponding flag on the knet handle on initialization, if
    set.
    
    Signed-off-by: Fabian Grünbichler <f.gruenbichler@proxmox.com>

--- exec/coroparse.c
+++ exec/coroparse.c
@@ -845,6 +845,14 @@ static int main_config_parser_cb(const char *path,
 					return (0);
 				}
 			}
+			if (strcmp(path, "system.allow_knet_asymmetric_routes") == 0) {
+				if ((strcmp(value, "yes") != 0) &&
+				    (strcmp(value, "no") != 0)) {
+					*error_string = "Invalid system.allow_knet_asymmetric_routes";
+
+					return (0);
+				}
+			}
 			break;
 
 		case MAIN_CP_CB_DATA_STATE_INTERFACE:
