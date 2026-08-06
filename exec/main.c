6e2f7ae4da2a71005ed2ffbb7fe38dd5ecdf7672
Jan Friesse <jfriesse@redhat.com>
2026-08-06 09:54:11 +0200

    crypto: Support for knet_get_crypto_hash_list
    Similar to the crypto cipher list, use
    knet_get_crypto_hash_list (if available) to get the list
    of allowed crypto hashes.
    
    For older versions of knet, a hardcoded list (identical to what was
    defined in coroparse.c) is used.
    
    The list is displayed in the corosync -v output and used by
    coroparse.c.
    
    Signed-off-by: Jan Friesse <jfriesse@redhat.com>
    Reviewed-by: Christine Caulfield <ccaulfie@redhat.com>

--- exec/main.c
+++ exec/main.c
@@ -1261,6 +1261,12 @@ static void show_version_info_crypto(void)
 	} else {
 		perror(error_string);
 	}
+
+	if (util_is_valid_knet_crypto_hash(NULL, &list_str, 1, "", &error_string) != -1) {
+		printf("Available crypto hashes: %s\n", list_str);
+	} else {
+		perror(error_string);
+	}
 }
 
 static void show_version_info_compress(void)
